bb8b10be0e0e8b66012080e7c845c141f7dc1e77
Ravinder Reddy Kothabad <developer.ravinder.reddy@gmail.com>
2025-01-22 13:39:14 +0530

    feat: add notes HELP.md

---
+++ src/main/java/com/paravar/learn/designPatterns/LearnDesignPatternsApplication.java
@@ -0,0 +1,13 @@
+package com.paravar.learn.designPatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnDesignPatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnDesignPatternsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paravar/learn/designPatterns/LearnDesignPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paravar.learn.designPatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnDesignPatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
